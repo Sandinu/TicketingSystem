5a643b6e502030eee2b4e143ccef599068295b0f
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2024-11-27 23:39:33 +0530

    Vendor Service initiated

--- Backend/src/main/java/com/sandinu/TicketingBackend/service/VendorService.java
+++ Backend/src/main/java/com/sandinu/TicketingBackend/service/VendorService.java
@@ -1,4 +1,17 @@
 package com.sandinu.TicketingBackend.service;
 
+import com.sandinu.TicketingBackend.model.Vendor;
+import com.sandinu.TicketingBackend.repo.VendorRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VendorService {
+    private final VendorRepo vendorRepo;
+
+    public VendorService(VendorRepo vendorRepo){
+        this.vendorRepo = vendorRepo;
+    }
+
+    public Vendor createVendor(Vendor)
+
 }
