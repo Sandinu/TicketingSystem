d97aecbb08cb0b562e12c19b56bff0e041427e0f
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2024-12-11 11:34:02 +0530

    Revert "supabase didnt workout"
    This reverts commit a57f33ce5271fbd30e179d1fbe83fd872ed6921f.

--- Frontend/ticketing-system/supabase/Supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Replace with your Supabase URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY; // Replace with your Supabase Anon Key
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
